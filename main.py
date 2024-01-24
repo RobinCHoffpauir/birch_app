359272aa92b67f058327d36dca05114fa108bc57
Robin C. Hoffpauir <91177807+RobinCHoffpauir@users.noreply.github.com>
2024-01-24 15:16:26 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 import tkinter as tk
 from gui.main_window import MainWindow
+from database import Database
 
 def main():
+    # Initialize the database
+    db = Database()
+
+    # Initialize the main application window
     root = tk.Tk()
-    root.title("Property Management App")
-    MainWindow(root)
+    root.title("Birch App")
+
+    # Create an instance of the main window class
+    main_window = MainWindow(root)
+
+    # Start the GUI event loop
     root.mainloop()
 
 if __name__ == "__main__":
